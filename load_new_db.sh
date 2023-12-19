c5e3a11bc94a793a4ce0395a1aaae75d22df0d1b
Patel kartik <67225313+patel-kartik-1@users.noreply.github.com>
2023-12-19 16:51:59 +0530

    Update load_new_db.sh

--- load_new_db.sh
+++ load_new_db.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
-#username 
+# MySQL credentials
 DB_USER="root"
 DB_PASSWORD="password"
+
+# Extract filename without extension
 FILE_NAME=$1
-DB_NAME="${FILE_NAME%.*}" # Extracting the filename without extension
+DB_NAME="${FILE_NAME%.*}"
 PWD_PATH=$(pwd)
 FULL_FILE_NAME="$PWD_PATH/$FILE_NAME"
 
-# Check if both arguments are provided
+# Check if the filename argument is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <file_name.sql>"
     exit 1
 fi
 
-# Log in to MySQL and execute commands
+# Load SQL file into MySQL
 mysql -u $DB_USER -p$DB_PASSWORD <<EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;
-SET GLOBAL innodb_strict_mode=0;
-USE $DB_NAME;
-SET GLOBAL innodb_strict_mode=0;
-source $FULL_FILE_NAME;
+CREATE DATABASE IF NOT EXISTS $DB_NAME;  # Create DB if it doesn't exist
+SET GLOBAL innodb_strict_mode=0;         # Disable strict mode globally
+USE $DB_NAME;                            # Use the created database
+SET GLOBAL innodb_strict_mode=0;         # Disable strict mode for the database
+source $FULL_FILE_NAME;                  # Load SQL file into the database
 EOF
 
-# Check the exit status of the MySQL command
+# Check if the first MySQL command was successful
 if [ $? -eq 0 ]; then
     echo "Database $DB_NAME loaded successfully."
     
-    # Execute the post-load SQL script
+    # Execute the pre-backup SQL script
     mysql -u $DB_USER -p$DB_PASSWORD <<EOF
     USE $DB_NAME;
     source $PWD_PATH/pre_backup_script.sql;
 EOF
 
-    # Check the exit status of the second MySQL command
+    # Check if the second MySQL command was successful
     if [ $? -eq 0 ]; then
         echo "Pre-backup script executed successfully."
     else
