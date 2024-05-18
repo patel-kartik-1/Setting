6c5a5d5516acc863c84c86dd863158ea5e030f8e
Patel kartik <67225313+patel-kartik-1@users.noreply.github.com>
2024-05-18 23:01:21 +0530

    Update load_new_db.sh

--- load_new_db.sh
+++ load_new_db.sh
@@ -1,45 +1,80 @@
 #!/bin/bash
-# MySQL credentials
+
+# Define colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 DB_USER="root"
 DB_PASSWORD="password"
-
-# Extract filename without extension
 FILE_NAME=$1
-DB_NAME="${FILE_NAME%.*}"
-PWD_PATH=$(pwd)
+DB_NAME="${FILE_NAME%.*}" # Extracting the filename without extension
+PWD_PATH="${2:-$(pwd)}"
+
+echo $1 $0 $PWD_PATH
+
 FULL_FILE_NAME="$PWD_PATH/$FILE_NAME"
 
-# Check if the filename argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <file_name.sql>"
+# Check if filename argument is provided
+if [ -z "$FILE_NAME" ]; then
+    echo -e "${RED}Usage: $0 <file_name.sql> [path]${NC}"
     exit 1
 fi
 
-# Load SQL file into MySQL
+# Check if the input file exists
+if [ ! -f "$FULL_FILE_NAME" ]; then
+    echo -e "${RED}Error: File '$FULL_FILE_NAME' not found.${NC}"
+    exit 1
+fi
+
+# Log in to MySQL and execute commands
 mysql -u $DB_USER -p$DB_PASSWORD <<EOF
-CREATE DATABASE IF NOT EXISTS $DB_NAME;  # Create DB if it doesn't exist
-SET GLOBAL innodb_strict_mode=0;         # Disable strict mode globally
-USE $DB_NAME;                            # Use the created database
-SET GLOBAL innodb_strict_mode=0;         # Disable strict mode for the database
-source $FULL_FILE_NAME;                  # Load SQL file into the database
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+USE $DB_NAME;
+SET innodb_strict_mode=0;
+source $FULL_FILE_NAME;
 EOF
 
-# Check if the first MySQL command was successful
+# Check the exit status of the MySQL command
 if [ $? -eq 0 ]; then
-    echo "Database $DB_NAME loaded successfully."
-    
-    # Execute the pre-backup SQL script
-    mysql -u $DB_USER -p$DB_PASSWORD <<EOF
-    USE $DB_NAME;
-    source $PWD_PATH/pre_backup_script.sql;
-EOF
+    echo -e "${GREEN}Database $DB_NAME loaded successfully.${NC}"
 
-    # Check if the second MySQL command was successful
+    echo -e "${YELLOW}Select an option:${NC}"
+    options=("${BLUE}DSM${NC}" "${BLUE}MDJ${NC}" "${BLUE}Other${NC}")
+
+    PS3=$(echo -e "${YELLOW}Enter the number corresponding to your System: ${NC}")
+    select choice in "${options[@]}"; do
+        case $REPLY in
+        1)
+            echo -e "${GREEN}You selected DSM${NC}"
+            # Execute the post-load SQL script
+            mysql -u $DB_USER -p$DB_PASSWORD $DB_NAME <$PWD_PATH/pre_backup_script_dsm.sql
+            break
+            ;;
+        2)
+            echo -e "${GREEN}You selected MDJ${NC}"
+            # Execute the post-load SQL script
+            mysql -u $DB_USER -p$DB_PASSWORD $DB_NAME <$PWD_PATH/pre_backup_script_mdj.sql
+            break
+            ;;
+        3)
+            echo -e "${GREEN}You selected Other System${NC}"
+            break
+            ;;
+        *)
+            echo -e "${RED}Invalid option, please try again.${NC}"
+            ;;
+        esac
+    done
+
+    # Check the exit status of the last MySQL command
     if [ $? -eq 0 ]; then
-        echo "Pre-backup script executed successfully."
+        echo -e "${GREEN}Pre-backup script executed successfully.${NC}"
     else
-        echo "Error executing pre-backup script."
+        echo -e "${RED}Error executing pre-backup script.${NC}"
     fi
 else
-    echo "Error loading database. Please check."
+    echo -e "${RED}Error loading database. Please check.${NC}"
 fi
